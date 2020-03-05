07c88746eeca726bfe35a43bedd8fe726db94c1d
Lucas Campos <lqccampos@gmail.com>
2020-03-05 16:28:53 +0100

    Bumped version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="vb_toolbox",
-    version="1.0.0",
+    version="1.0.1",
     author="Lucas da Costa Campos",
     author_email="lqccampos@gmail.com",
     description="Library and command-line tool to calculate the Vogt-Bailey index of a dataset",
