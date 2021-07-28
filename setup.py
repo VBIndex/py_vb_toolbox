f49c588867ae2ba2a82477cd56eb4b839e8dea60
Claude J Bajada <16142659+claudebajada@users.noreply.github.com>
2021-07-28 16:20:56 +0200

    Update setup.py
    Changed version number to 2.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="vb_toolbox",
-    version="2.0.0",
+    version="2.1.0",
     author="Lucas da Costa Campos",
     author_email="lqccampos@gmail.com",
     description="Library and command-line tool to calculate the Vogt-Bailey index of a dataset",
