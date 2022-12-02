ac3bd34bd22a89bb30121d4fe66530487aa66526
Jascha S <jascha.schewtschenko@port.ac.uk>
2022-12-02 15:07:51 +0000

    fixes

--- test/test_numerics.py
+++ test/test_numerics.py
@@ -9,12 +9,12 @@ from vb_toolbox import numerics
 
 class Numerics_TestCase(TestCase):
 
-    def test_force_numerics(self):
+    def test_force_symmetric(self):
         N = np.random.randint(low=2, high=200, size=10)
         for n in N:
             test_arr = np.random.rand(n,n)
             ret = numerics.force_symmetric(test_arr)
-            assert_array_equal(np.diag(ret), np.diag(test_arr))
+            assert_array_equal(np.triu(ret), np.triu(test_arr))
             assert_array_equal(ret, ret.T)
 
     def test_get_fiedler_eigenpair(self):
