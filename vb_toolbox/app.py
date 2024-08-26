750bbb55eecfff4b6cd62c50f6f37dc4da360e2d
Claude J Bajada <16142659+claudebajada@users.noreply.github.com>
2024-08-26 13:25:15 +0200

    Update app.py
    Removed some excess information at the top of the file. Very minor change.

--- vb_toolbox/app.py
+++ vb_toolbox/app.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb 28 11:48:15 2024
-
-@author: bobducks
-"""
-
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 # vim:fenc=utf-8
