5585187f9ce4093d9c7fd1976b3a87e40cbb7e92
Stephan Heunis <s.heunis@fz-juelich.de>
2024-12-03 14:45:59 +0100

    Adds a basic automatic documenation setup
    This uses sphinx and shinx-book-theme.
    The index includes references to overview, instalation, and usage pages
    but these are just general placeholders and can be changed as necessary.
    Developer requirements and a readme for contributing to docs are also
    included, with accompanying instructions for local rendering.

---
+++ docs/source/conf.py
@@ -0,0 +1,34 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Vogt-Bailey Index Toolbox'
+copyright = '2024, vbtoolbox developers'
+author = 'vbtoolbox developers'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_book_theme'
+html_static_path = ['_static']
+html_theme_options = {
+    "logo": {
+        "text": "Vogt-Bailey Index Toolbox",
+        "image_light": "_static/vb_toolbox_logo.png",
+        "image_dark": "_static/vb_toolbox_logo.png",
+    }
+}
